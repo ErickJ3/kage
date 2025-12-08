f57cded54905cf977b861d617b7bb2bcb5143a6b
Erick Jesus <erick.jesus2060@gmail.com>
2025-12-08 18:03:23 -0300

    feat: remove-built in-middlewares

--- packages/core/src/middleware/mod.ts
+++ packages/core/src/middleware/mod.ts
@@ -1,15 +1,6 @@
 /**
- * Middleware module - composition and built-in middleware.
+ * Middleware module
  */
 
 export type { Middleware } from "~/middleware/types.ts";
 export { compose } from "~/middleware/compose.ts";
-
-export {
-  compression,
-  type CompressionOptions,
-  cors,
-  type CorsOptions,
-  errorHandler,
-  logger,
-} from "~/middleware/builtin/mod.ts";
